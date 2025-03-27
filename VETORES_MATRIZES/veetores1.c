d8b43e7372e6fc8dae7cbef1d040167398c6f221
Gabriel <gabrielgoncalves.dev@gmail.com>
2025-03-26 23:28:24 -0300

    .

---
+++ VETORES_MATRIZES/veetores1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define N_NOTAS 10
+
+void media(int notas[N_NOTAS]) {
+	int notasTotal = 0;
+	for (int i = 0; i < N_NOTAS; i++) {
+		notasTotal += notas[i];
+	}
+	float media = (float)(notasTotal) / N_NOTAS;
+	int cont = 0;
+	for (int i = 0; i < N_NOTAS; i++) {
+		if (media < notas[i]) {
+			cont++;
+		}
+	}
+	printf("A media da turma e: %g\nO numero de alunos que passaram sao: %d", media, cont);
+}
+
+void preencher(int notas[N_NOTAS]) {
+
+	for (int i = 0; i < N_NOTAS; i++) {
+		scanf("%d", &notas[i]);
+	}
+	media(notas);
+}
+
+int main(int argc, char const* argv[]) {
+
+	int notas[N_NOTAS];
+	preencher(notas);
+
+	return 0;
+}
