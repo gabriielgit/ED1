e3c359dcbdba82cbfc28f688a072583689a48331
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2025-06-05 23:45:25 +0000

    .

--- lista_simples.c
+++ lista_simples.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
+void menu() {
+	char op;
 
+	do {
+		printf("\nA - Insersão em lista - Pelo Início"
+			   "\nB - Insersão em lista - Pelo Fim"
+			   "\nC - Excluir"
+			   "\nD - Imprimir"
+			   "\nE - Finalizar"
+			   "\nDigite sua opção: ");
+		scanf(" %c", &op);
+		op = toupper(op);
 
+		switch (op) {
+		case 'A':
+			break;
+		case 'B':
+			break;
+		case 'C':
+			break;
+		case 'D':
+			break;
+		case 'E':
+			printf("\nPrograma finalizado.");
+			break;
+		default:
+			printf("\nOpção inválida.");
+		}
 
-int main()
-{
-	
-
-
-
-	return 0;
+	} while (op != 'E');
 }
