4b63ef2bef18d261355f05633c370c0daa6a29c7
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2025-06-12 23:55:53 +0000

    .

---
+++ ListaDupla.c
@@ -0,0 +1,8 @@
+
+#include "biblioteca.h"
+
+int main(void) {
+    setbuf(stdout, NULL);
+    menu();
+    return 0;
+}
---
+++ biblioteca.h
@@ -0,0 +1,26 @@
+
+#ifndef BIBLIOTECA_H_
+#define BIBLIOTECA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ALEATORIO() rand() % 101
+
+typedef struct dados listaDupla;
+
+struct dados {
+    int codigo;
+    listaDupla *anterior;
+    listaDupla *proximo;
+};
+
+void menu();
+listaDupla* inserirInicio(listaDupla *inicio);
+listaDupla* inserirFim(listaDupla *inicio);
+listaDupla* pesquisarNo(int codigo, listaDupla* lista);
+listaDupla* excluirNo(listaDupla *lista);
+void exibirLista(listaDupla *lista);
+int digitarCodigo();
+
+#endif /* BIBLIOTECA_H_ */
