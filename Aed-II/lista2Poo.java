fa7128d920fd2959151a8da14cb20fee385791eb
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2026-02-25 22:38:17 +0000

    .

---
+++ Aed-II/lista2Poo.java
@@ -0,0 +1,34 @@
+import Java.util.Scanner;
+
+public class lista2Poo {
+	class Veiculo {
+		int Codigo;
+		String Placa;
+		String Modelo;
+	}
+
+	class DataEntrada {
+		int Dia;
+		int Mes;
+		int Ano;
+		int hora;
+		int Minuto;
+	};
+
+	class DataSaida {
+		int Dia;
+		int Mes;
+		int Ano;
+		int hora;
+		int Minuto;
+	};
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int idade;
+		String nome;
+
+	}
+}
