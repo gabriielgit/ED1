1cdab4106f83583ff53aecb517e8bcc7f7ae9c0a
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2026-03-04 22:30:12 +0000

    .

--- Aed-II/lista3.java
+++ Aed-II/lista3.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class lista3 {
 	public static void main(String[] args) {
+		public static int (int numero){
+
+
+		}
 
 public static double divisao(int a, int b) {
 
 	public static void main(String[] args) {
+		double d = divisao(4, 0);
+		System.out.println(d);
 
 	}
+
+	public static void main(String[] args) {
+	
 }
