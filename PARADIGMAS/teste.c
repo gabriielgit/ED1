25a2aa0c372fac41fc11989dbdc73b62a727f4ab
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2026-08-25 22:23:17 +0000

    .

--- PARADIGMAS/teste.c
+++ PARADIGMAS/teste.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main() {
+int main() {
 
+	char nome[20];
+	int idade;
+	printf("Digite o nome: ");
+	scanf("%[^\n]", nome);
+	printf("Digite a idade: ");
+	scanf("%d", &idade);
+	printf("Nome: %s, %d anos", nome, idade);
 	return 0;
 }
