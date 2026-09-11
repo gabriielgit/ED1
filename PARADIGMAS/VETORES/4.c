c5ca35fdf0157106f60408ab2a1be93316ffb43b
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-11 17:50:21 -0300

    .

--- PARADIGMAS/VETORES/4.c
+++ PARADIGMAS/VETORES/4.c
@@ -4,6 +4,36 @@
 
 int main() {
 	system("cls");
+	int x = 10, y = 15;
+	int vet_1[x];
+	int vet_2[y];
+	int vet_3[x + y];
+
+	srand(time(NULL));
+	for (int i = 0; i < x; i++) {
+		vet_1[i] = rand() % 10 + 1;
+	}
+	for (int i = 0; i < y; i++) {
+		vet_2[i] = rand() % 10 + 1;
+	}
+
+	for (int i = 0; i < x; i++) {
+		if (i != x) {
+			vet_3[i] = vet_1[i];
+		}
+	}
+	for (int i = 0; i < y; i++) {
+		vet_3[i + x + 1] = vet_2[i];
+	}
+	for (int i = 0; i < y; i++) {
+		vet_3[i + x + 1] = vet_2[i];
+	}
+	for (int i = 0; i < y; i++) {
+		vet_3[i + x + 1] = vet_2[i];
+	}
+	for (int i = 0; i < y; i++) {
+		vet_3[i + x + 1] = vet_2[i];
+	}
 
 	return 0;
 }
