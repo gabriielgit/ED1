620eb74d43206a1b7f6c99a9aa8e0a5710a33611
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-11 16:34:12 -0300

    .

--- PARADIGMAS/VETORES/1.C
+++ PARADIGMAS/VETORES/1.C
@@ -3,41 +3,6 @@
 
 int main() {
 	system("cls");
-	int tam = 10;
-	int vet[tam], troca = 0;
-
-	// ORDENA
-	for (int i = 0; i < tam; i++) {
-		scanf("%d", &vet[i]);
-	}
-	for (int j = 1; j <= tam; j++) {
-		for (int i = 0; i < tam - j; i++) {
-
-			if (vet[i] > vet[i + 1]) {
-				troca = vet[i];
-				vet[i] = vet[i + 1];
-				vet[i + 1] = troca;
-			}
-		}
-	}
-
-	// REMOVE AS REPETIDAS
-	int k = 0, cont = 0;
-	for (k = 0; k < tam; k++) {
-		if (vet[k] == vet[k + 1]) {
-			for (k; k < tam; k++) {
-				vet[k + 1] = vet[k + 2];
-			}
-			cont++;
-		}
-	}
-
-	// PRINTA
-	system("cls");
-	for (int i = 0; i < tam - cont; i++) {
-
-		printf("%d\n", vet[i]);
-	}
 
 	return 0;
 }
