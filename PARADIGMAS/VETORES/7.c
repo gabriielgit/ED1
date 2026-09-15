a5cca115656756b8612399836e3fcd532d332daf
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2026-09-15 21:07:36 +0000

    .

--- PARADIGMAS/VETORES/7.c
+++ PARADIGMAS/VETORES/7.c
@@ -4,13 +4,29 @@
 
 int main() {
 	system("cls");
-	int x = 10;
+	int x = 10, maior, pos;
+	;
 	int vet[x];
 
 	srand(time(NULL));
 	for (int i = 0; i < x; i++) {
 		vet[i] = rand() % 10 + 1;
 	}
+
+	for (int i = 0; i <= x - 1; i++) {
+		if (vet[i] > vet[i + 1]) {
+			maior = vet[i];
+			pos = i;
+		};
+	}
+
+	for (int i = 0; i < x; i++) {
+		printf("%d\n", vet[i]);
+	}
+
+	printf("\n");
+
+	printf("Maior: %d\nPosicao: %d\n", maior, pos);
 
 	return 0;
 }
