224915d8ea4bc4d42879be11b4ec571c06174afa
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-15 16:16:09 -0300

    .

--- PARADIGMAS/VETORES/6.c
+++ PARADIGMAS/VETORES/6.c
@@ -4,6 +4,26 @@
 
 int main() {
 	system("cls");
+	int x = 10;
+	int vet_1[x];
+	int vet_2[x];
+	int vet_3[x * 2];
 
+	srand(time(NULL));
+	for (int i = 0; i < x; i++) {
+		vet_1[i] = rand() % 10 + 1;
+	}
+
+	for (int i = 0; i < x; i++) {
+		vet_2[i] = rand() % 10 + 1;
+	}
+
+	for (int i = 0; i < x * 2; i++) {
+		if (x % 2 == 0) {
+			vet_3[i] = vet_1[i];
+		} else {
+			vet_3[i] = vet_2[i];
+		}
+	}
 	return 0;
 }
