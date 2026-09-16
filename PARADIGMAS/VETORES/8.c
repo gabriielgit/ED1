67ef6dc846008f456abdfc84799a21de707f8abe
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-16 13:33:54 -0300

    .

--- PARADIGMAS/VETORES/8.c
+++ PARADIGMAS/VETORES/8.c
@@ -5,16 +5,18 @@
 
 int main() {
 	system("cls");
-	int x = 40, maior, menor, maior_cont = 0, menor_cont = 0;
+	long long int x = 99999999999, maior, menor, maior_cont = 0, menor_cont = 0;
 	double media = 0;
-	int vet[x];
+	int* vet = malloc(x * sizeof(int));
+	clock_t fim, inicio;
+	inicio = clock();
 
 	srand(time(NULL));
-	for (int i = 0; i < x; i++) {
+	for (long long int i = 0; i < x; i++) {
 		vet[i] = rand() % 20 + 1;
 	}
 
-	for (int i = 0; i < x; i++) {
+	for (long long int i = 0; i < x; i++) {
 		if (vet[i] > 16) {
 			maior_cont++;
 
@@ -31,6 +33,7 @@ int main() {
 	}
 
 	printf("%d menores ou iguais a 16 anos,\n%d maiores que 16 anos,\n%.2f idade media, \n%d maior idade, \n%d menor idade\n", maior_cont, menor_cont, media /= x, maior, menor);
-
+	printf("TEMPO: %f\n", (double)(fim = clock() - inicio) / CLOCKS_PER_SEC);
+	free(vet);
 	return 0;
 }
