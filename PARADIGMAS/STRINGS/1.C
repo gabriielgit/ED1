a59edf402896d154f8736d4d760f25ae084ae812
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-22 15:44:59 -0300

    .

--- PARADIGMAS/STRINGS/1.C
+++ PARADIGMAS/STRINGS/1.C
@@ -1,12 +1,15 @@
-#include <String.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 	system("cls");
 	char str[15], str1[20];
 	fgets(str, 15, stdin);
+	str[strcspn(str, "\n")] = '\0';
+	fflush(stdin);
 	fgets(str1, 20, stdin);
+	fflush(stdin);
 	strcat(str, str1);
 	fputs(str, stdout);
 	return 0;
