35bdc7b5d8267844bbb544232d0173ca61ba92dc
Gabriel <99220061+gabriielgit@users.noreply.github.com>
2026-09-22 22:05:21 +0000

    .

--- PARADIGMAS/STRINGS/6.c
+++ PARADIGMAS/STRINGS/6.c
@@ -5,15 +5,11 @@
 int main() {
 	system("cls");
 	int cont = 0;
-	char str[15];
+	char str[40];
 	fgets(str, 15, stdin);
 	str[strcspn(str, "\n")] = '\0';
 	fflush(stdin);
-	for (int i = 0; i < strlen(str) != '\0'; i++) {
-		if (str[i] == 32) {
-			cont++;
-		}
-	}
+
 	fputs(str, stdout);
 	printf("\n%d", cont);
 	return 0;
