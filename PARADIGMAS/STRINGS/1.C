fd1bf6ed04d0def8167b1d54fd0b71ffb2075a0b
gabriel <gabrielgoncalves.dev@gmail.com>
2026-09-22 14:39:04 -0300

    .

--- PARADIGMAS/STRINGS/1.C
+++ PARADIGMAS/STRINGS/1.C
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 
 int main() {
-
+	system("cls");
+	char str[15], str1[20];
+	fgets(str, 15, stdin);
+	fgets(str1, 20, stdin);
+	strcat(str, str1);
+	fputs(str, stdout);
 	return 0;
 }
